3cbfa56a17a5318a141ee31996bebdd7e7e5926a
lmwenda <lukemwen@hotmail.com>
2025-04-23 21:52:33 +0100

    Everything to work on Blogging :hammer:

--- frontend/src/utils/s3.js
+++ frontend/src/utils/s3.js
@@ -1,13 +1,8 @@
 import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
-<<<<<<< HEAD
-  accessKeyId: ,
-  secretAccessKey: 
-=======
   accessKeyId: "",
   secretAccessKey: ""
->>>>>>> 0d39baa (Everything to work on Blogging :hammer:)
 });
 
 export default s3;
